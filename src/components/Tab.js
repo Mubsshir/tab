9dddd7f297ba34c5407e8648264e284b97708ef9
Mubsshir <39831189+Mubsshir@users.noreply.github.com>
2022-11-30 13:05:58 +0530

    problem fixed where i'm getting undeifned array.and data is now fetched from the api.

--- src/components/Tab.js
+++ src/components/Tab.js
@@ -1,33 +1,37 @@
 import styles from './Tab.module.css';
 import { FaAngleDoubleRight } from 'react-icons/fa';
-import { useState } from 'react';
-import data from './data';
-//const url = "https://course-api.com/react-tabs-project";
+import { useEffect, useState } from 'react';
+const url = "https://course-api.com/react-tabs-project";
 
 const Tab = () => {
     const [index, setIndex] = useState(0);
+    const [loading, setLoading] = useState(false);
+    const [jobData, setJobData] = useState([]);
 
-    // const fetchData = async () => {
-    //     console.log("I run")
-    //     try {
-    //         const res = await fetch(url);
-    //         if (!res.ok) {
-    //             console.log("OOPs")
-    //             throw new Error("Somthing went wrong");
-    //         }
+    const fetchData = async () => {
+        setLoading(true);
+        try {
+            const res = await fetch(url);
+            const data = await res.json();
+            setJobData(data);
+            setLoading(false);
+        }
+        catch (err) {
+            console.log(err.message);
+        }
 
-    //         const data = await res.json();
-    //         setEmpData(data);
-    //     } catch (err) {
-    //         console.log(err.message);
-    //     }
-    // }
-
-    return (
+    }
+    useEffect(() => {
+        fetchData()
+    }, []);
+    if (loading) {
+        return <h3>Loading.....</h3>
+    }
+    return ((jobData.length > 0 && !loading) &&
         <div className={styles.Tab}>
             <section className={styles.nameList}>
                 <ul>
-                    {data.map((emp, ind) => <li
+                    {jobData.map((emp, ind) => <li
                         className={index === ind ? styles.active : ""}
                         key={emp.id}
                         onClick={() => { setIndex(ind) }}
@@ -37,11 +41,11 @@ const Tab = () => {
                 </ul>
             </section>
             <section className={styles.details}>
-                <h3>{data[index].title}</h3>
-                <h3 className={styles._name}>{data[index].company}</h3>
-                <p className={styles.date}>{data[index].dates}</p>
+                <h3>{jobData[index].title}</h3>
+                <h3 className={styles._name}>{jobData[index].company}</h3>
+                <p className={styles.date}>{jobData[index].dates}</p>
                 <div className={styles.about}>
-                    {data[index].duties.map((duty, pos) => <p key={pos}><FaAngleDoubleRight className={styles.angle} />{duty}</p>)}
+                    {jobData[index].duties.map((duty, pos) => <p key={pos}><FaAngleDoubleRight className={styles.angle} />{duty}</p>)}
                 </div>
             </section>
         </div >
